fdcfb6f1c6cdfeec1cd610e8f8ac5836aeffd9cd
Hugo Ribeiro <hugofribeiro@gmail.com>
2011-05-25 23:21:51 +0100

    Alterações 25.Maio

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -39,7 +39,8 @@ echo $OUTPUT->doctype() ?>
             echo $OUTPUT->lang_menu();
             echo $PAGE->headingmenu;
         ?></div>
-        <h1 class="headermain"><!-- <?php echo $PAGE->heading ?> --><img src="http://localhost/moodle/theme/up/pix/logo.png" alt="logo" width="413" height="60"/></h1>
+        <h1 class="headermain"><!-- <?php echo $PAGE->heading ?> -->
+        <img src="http://localhost/moodle/theme/up/pix/logo.png" alt="logo" width="413" height="60"/></h1>
         <?php if ($hascustommenu) { ?>
         <div id="custommenu"><?php echo $custommenu; ?></div>
         <?php } ?>
