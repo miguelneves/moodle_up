e48b51279f0ea0c7958e10c426aa8356dd03cd53
unknown <MIGUELNEVES@.(none)>
2011-08-15 22:04:10 +0100

    fix sub menu

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	
 	//init
 	customMenu();
-	textIcons();
-	
+	tableContents();
+	textIcons();	
 	if($('body').hasClass('editing')) {
 		editMode();
 	}		
@@ -11,6 +11,10 @@ $(document).ready(function() {
 
 function customMenu() {
 	var content = '';
+	
+	//hide default block
+	$('.block_navigation').hide();
+	$('.block_settings').hide();
 	
 	//remove images
 	$('.block_navigation a img').remove();
@@ -200,7 +204,6 @@ function parseItem(item, name){
 	} else {
 		var level = (name || 0);
 	}
-	
 	level++;
 	
 	var h4 = $('> p a, > p > span', item);
@@ -222,7 +225,7 @@ function parseItem(item, name){
 	var branchContent = new Array();
 	var noBranchContent = '';	
 	var i = 0;
-	
+
 	$(list).each( function(index){
 		if($(this).hasClass('contains_branch') && !$(this).hasClass('type_course')) {
 			branchContent[i] = parseItem(this, level)	
@@ -231,10 +234,12 @@ function parseItem(item, name){
 			noBranchContent += '<li role="menuitem">' + $('> p', this).html() + '</li>';		
 		}		
 	});
-	
-	if(!$(this, list).hasClass('type_course')) {
-		if (level == 1)
-			noBranchContent = '<li class="general"><ul>' + noBranchContent + '</ul></li>';
+	//test if noBranchContent is empty
+	if(noBranchContent != ''){
+		if(!$(this, list).hasClass('type_course')) {
+			if (level == 1)
+				noBranchContent = '<li class="general"><ul>' + noBranchContent + '</ul></li>';
+		}
 	}
 	//output only filled elements
 	branchTemp = "";
@@ -251,164 +256,18 @@ function parseItem(item, name){
 	if (level == 1)
 		list = '<ul class="sub menu-group-collapsed" role="group">' + branch + '</ul>';
 	else
-		list = '<ul>' + branch +  '</ul>';
+		list = '<ul>' + branch + '</ul>'
 
 	if (level == 1)
 		content = '<li id="' +  id + '" class="level-' + level + '" role="menuitem">' + h4 + list +  '</li>';
 	else
 		content = '<li class="level-' + level + '" role="menuitem expanded">' + h4 + list +  '</li>';
-	
+		
 	return content;
 }
 
 
-function customMenu2(){
-	
-	//remove default tree view
-	$('.block_navigation .block_tree').removeClass();
-	$('.block_settings .block_tree').removeClass();
-	
-	//removeove images
-	$('.block_navigation a img').remove();
-	$('.block_settings a img').remove();
-	
-	
-	//MEGAMENU
-	/*
-	 * Home
-	 * My Profile
-	 * My Courses
-	 * Settings
-	 * 
-	 * Site Pages?
-	 */
-	
-	//get content
-	var home = $('.block_navigation a[title="My home"]');
-	var myProfile = $('.block_navigation li.depth_2:eq(2)');
-	var myCourses = $('.block_navigation li.depth_2:eq(3)');
-	var settings = $('.block_navigation a[title=Home]');
-	
-	//create megamenu if exist
-	if($(home).length) {	
-	
-		//MY PROFILE
-		var h3 = $('> p span', myProfile);
-		var ul = $('> ul', myProfile);
-		
-		//contain branch
-		var h4 = $('> li.contains_branch p span', ul);
-		var branch = $('> li.contains_branch ul', ul);
-	
-		//clean up branch
-		var branchContent = new Array(3);
-		for (var i = branchContent.length - 1; i >= 0; i--) branchContent[i] = '';
-	
-		$(branch).each( function(index){
-			branchTemp = '';
-			$('li p', this).each( function(){
-				branchTemp += '<li>' + $(this).html() + '</li>';
-			})
-			branchContent[index] +=  branchTemp;
-		});
-	
-		//create branch
-		content = '';
-		for (var i = branch.length - 1; i >= 0; i--){
-			content += '<li><a href="#"><h4>' + h4[i].innerHTML + '</h4></a>';
-			content += '<ul>' + branchContent[i] + '</ul></li>';
-		};
-		
-		//no branch
-		var noBranch = $('> li.item_with_icon p', ul);
-		noBranchContent = '';	
-		$(noBranch).each( function(){
-			noBranchContent += '<li>' + this.innerHTML + '</li>';
-		});
-	
-		//noBranchContent = '<li class="general"><ul>' + noBranchContent + '</ul></li>';
-		noBranchContent = '<li class="general"><a href="#"><h4>General</h4></a><ul>' + noBranchContent + '</ul></li>';
-			//noBranchContent = '<li id="general"><a href="#"><h4>General</h4></a><ul>' + noBranchContent + '</ul></li>';
-
-		content = noBranchContent + content;
-		
-		//create my profile content
-		mp = '<a href="#">' + h3.html() + '</a><ul class="sub">' + content + '</ul>';
-		//mp = '<a href="#"><h3>' + h3.html() + '</h3></a><ul class="sub">' + content + '</ul>';
-		
-		//MY COURSES
-		
-		//get courses
-		var courses = $('> ul', myCourses);
-		//get my courses title
-		var h3 = $('> p span', myCourses);
-		var ul = '';
-		
-		//create list of courses
-		$('> li', courses).each( function(){
-			li = $('> .tree_item', this);
-			ul += '<li>' + li[0].innerHTML + '</li>';
-		});
-		
-		//create my course content
-		mc = '<a href="#">' + h3.html() + '</a><ul class="sub">' + ul + '</ul>';
-		//mc = '<a href="#"><h3>' + h3.html() + '</h3></a><ul class="sub">' + ul + '</ul>';
-		//mc = '<a>' + a.html() + '</a><ul>' + ul + '</ul>';
-		
-		$('.block_navigation').hide();
-		
-		//SETTINGS
-		//get settings
-		var settings = $('.block_settings');
-		
-		//get settings title
-		var h3 = $('.header .title h2', settings);
-		var set = $('#settingsnav > ul > li', settings);
-		var set2 = $('#settingsnav > ul > li .contains_branch', settings);
-
-		s = getBranch(set);
-		s += getBranch(set2);
-		
-		//create settings content
-		set = '<a href="#">' + h3.html() + '</a><ul class="sub">' + s + '</ul>';
-		//set = '<a href="#"><h3>' + h3.html() + '</h3></a><ul class="sub">' + s + '</ul>';
-		
-		//Create megadropdown menu		
-		if($(home).length)
-			list = '<li id="home" class="first-level">' + home.parent().html() + '</li>';
-			//list = '<li id="home" class="first-level"><h3 id="myHome">' + home.parent().html() + '</h3></li>';
-		if($(myProfile).length)
-			list += '<li id="myProfile" class="first-level submenu">' + mp + '</li>';
-		if($(myCourses).length)
-			list += '<li id="myCourses" class="first-level submenu">' + mc + '</li>';
-		if($(settings).length)
-			list += '<li id="settings" class="first-level submenu">' + set + '</li>';
-		
-		$('#page-header').append('<div id="megamenu"><ul id="menu">' + list + '</ul></div>');
-		
-		
-		//megadropdown behaviour
-		$('#megamenu .sub').hide();
-		$('#megamenu li a').click(function(ev){
-			$('#megamenu li .sub').slideUp('fast', function(){
-				$(this).parent().removeClass('active');
-			});
-			if($(this).next().css('display') == 'none') {
-				$(this).next().slideDown('fast').parent().addClass('active');
-			}
-			//ev.preventDefault();
-		});
-		
-		//remove menu
-		$(document.body).bind('click', function() {
-			$('#megamenu li .sub').slideUp('fast', function(){
-				$(this).parent().removeClass('active');
-			}); 
-		});
-		$('#megamenu .first-level').bind('click', function(ev) {
-			ev.stopPropagation();
-		});
-	}
+function tableContents(){
 	
 
 	//NEW NAVIGATION BLOCK 
@@ -447,42 +306,14 @@ function customMenu2(){
 		return false;
 	});
 
-	// create lists
-	function getBranch(el) {
-		content = '';
-	
-		el.each(function(){
-			var h4 = $('> p > a, > p > span', this);
-			var ul = $('> ul', this);
-			var lista = '';
-	
-			$('> li', ul).each(function(){
-				lista += '<li>' + $('p', this).html() + '</li>';
-			});
-			
-			//create branch
-			branch = '';
-			branch += '<li class="level-2"><a href="#" class="list-header"><h4>' + h4.html() + '</h4></a>';
-			branch += '<ul>' + lista + '</ul></li>';
-		
-			content += branch;
-		});
-		
-		return content;
-	}
-
-
 }
 
 //CHANGE ICONS TO TEXT
 function textIcons(){
-	
 	//course content edit mode
 	$('.course-content .commands a').each(function(){
 		iconToText(this, true);
 	});
-	
-	
 }
 //change link icons to text
 function iconToText(icon, remove){
