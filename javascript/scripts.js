31ee848feed06d9733a386a738d3ca70fc9a5506
Miguel Neves <miguelnvs@gmail.com>
2012-06-23 18:34:13 +0100

    added icon to block title when on edit mode

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -29,27 +29,9 @@ $(document).ready(function() {
 		pause: 4000,
 		controls: false,
 		randomStart: false
-//		onAfterSlide: function(currentSlideNumber, totalSlideQty, currentSlideHtmlObject){
-//			currentSlideHtmlObject.parent().parent().css('width', '100%').parent().css('width', '100%'); // use something more pretty
-//		}
 	});
-	     //Calculate the height of <header>
-	       //Use outerHeight() instead of height() if have padding
-	       var aboveHeight = $('.block_settings').outerHeight();
 
-		//when scroll
-	       $(window).scroll(function(){
-		        //if scrolled down more than the header’s height
-	               if ($(window).scrollTop() > 200){
 
-		        // if yes, add “fixed” class to the <nav>
-		        // add padding top to the #content                (value is same as the height of the nav)
-//	               $('.block_settings').addClass('fixed');
-
-	               } else {
-//	               $('.block_settings').removeClass('fixed');
-	               }
-	       });
 
 });
 
@@ -75,19 +57,12 @@ function customMenu() {
 	myCourses.find('.type_course').removeClass('contains_branch');
 	myCourses.find('.type_course ul').remove();
 
-	/*
-var settingsName = $('.block_settings .header .title h2').html()
-	var settings = $('#settingsnav > ul');
-	settings = $('<li class="contains_branch"></li>').append(settings);
-*/
-
 
 	if(myProfile.length){
 		content = '<li id="home" class="level-1" role="menuitem">' + myHome.html() + '</a></li>'
 		+ parseItem(myProfile, 'profile')
 		+ parseItem(myCourses, 'courses')
 		+ parseItem(sitePages, 'pages');
-		//+ parseItem(settings, settingsName);
 
 		$('#page-header').append('<div id="megamenu"><ul id="menu" class="menu" role="menu">' + content + '</ul></div>');
 
@@ -111,7 +86,6 @@ var settingsName = $('.block_settings .header .title h2').html()
 					.attr('aria-expanded', 'false')
 					.find('>a')
 					.addClass('menu-parent menu-parent-collapsed');
-		//	menu.find('li.first-level > ul > li').attr('aria-expanded','true');
 			menu.find('li.level-1 > ul > li').has('ul').addClass('expanded');
 
 		//bind custom events
@@ -185,9 +159,7 @@ var settingsName = $('.block_settings .header .title h2').html()
 					menu.find('[tabindex=0]').attr('tabindex','-1');
 					//assign 0 tabindex to focused item
 					$(event.target).attr('tabindex','0');
-					//if($('li.first-child').hasClass('menu-item-active') && $('li.first-child').not('[aria-expanded=true]'))
 					$(event.target).parent().siblings('li.level-1').find('> a').trigger('collapse');
-				//	$(event.target).parent().next('li.level-1').find('> a').trigger('collapse');
 			},
 			click: function(event){
 				//save reference to event target
@@ -270,79 +242,6 @@ function parseItem(item, name){
 	var list = $('> ul', item);
 
 	var content = '<li class="level-1 ' + name + '"><a href="#" tabindex="-1" class="menu-parent menu-parent-collapsed">' + h4.html() + '</a><ul class="sub menu-group-collapsed">' + list.html() + '</ul></li>';
-
-//	if (level == 1) {
-//		h4 = '<a href="#" tabindex="-1" class="menu-parent menu-parent-collapsed">' + h4 + '</a>';
-//	} else {
-//		h4 = '<a href="#" tabindex="-1"><h4>' + h4 + '</h4></a>';
-//	}
-
-//	if (level == 1)
-//	var content = '<li id="' + /* id +*/ '" class="level-' + level + '" role="menuitem">' + h4 + list +  '</li>';
-
-
-//	else
-//		content = '<li class="level-' + level + '" role="menuitem expanded">' + h4 + list +  '</li>';*/
-	/*level++;
-
-	var h4 = $('> p a, > p > span', item);
-	var list = $('> ul > li', item);
-
-	//define list id
-	var id = h4.text().replace(/ /g,'-') || name;
-	id = id.toLowerCase();
-
-	//convert h4 to html
-	h4 = h4.html() || name;
-
-	if (level == 1)
-		h4 = '<a href="#" tabindex="-1" class="menu-parent menu-parent-collapsed">' + h4 + '</a>';
-	else
-		h4 = '<a href="#" tabindex="-1"><h4>' + h4 + '</h4></a>';
-
-	//define list content
-	var branchContent = new Array();
-	var noBranchContent = '';
-	var i = 0;
-
-	$(list).each( function(index){
-		//if($(this).hasClass('contains_branch') && !$(this).hasClass('type_course')) {
-		if($(this).hasClass('contains_branch') && !$(this).hasClass('depth_3')) {
-			branchContent[i] = parseItem(this, level)
-			i++;
-		} else {
-			noBranchContent += '<li role="menuitem">' + $('> p', this).html() + '</li>';
-		}
-	});
-	//test if noBranchContent is empty
-	if(noBranchContent != ''){
-		if(!$(this, list).hasClass('type_course')) {
-			if (level == 1)
-				noBranchContent = '<li class="general"><ul>' + noBranchContent + '</ul></li>';
-		}
-	}
-	//output only filled elements
-	branchTemp = "";
-	for (var i=0; i < branchContent.length; i++) {
-		if (branchContent[i] != null)
-			branchTemp += branchContent[i];
-	};
-
-	branchContent = branchTemp
-
-	branch = noBranchContent + branchContent;
-
-	//set lists
-	if (level == 1)
-		list = '<ul class="sub menu-group-collapsed" role="group">' + branch + '</ul>';
-	else
-		list = '<ul>' + branch + '</ul>'
-
-	if (level == 1)
-		content = '<li id="' +  id + '" class="level-' + level + '" role="menuitem">' + h4 + list +  '</li>';
-	else
-		content = '<li class="level-' + level + '" role="menuitem expanded">' + h4 + list +  '</li>';*/
-
 	return content;
 }
 
@@ -361,44 +260,6 @@ function settingsMenu(){
 
 	$('#region-main-wrap').append(settings);
 
-}
-
-function tableContents(){
-
-	//NEW NAVIGATION BLOCK
-
-	//DEFINE LISTS
-	//table of contents
-	var tableContents = $('.block_navigation .type_course li.type_structure');
-	$('.course-content .outline').remove();
-
-	var outline = $('.course-content .outline').html();
-	if(outline) {
-		var tc = '';
-		$('p span', tableContents).each(function(index){
-			tc += '<li class="tc"><a href="#section-' + index + '">' + $(this).html() + '</a></li>';
-		});
-
-		//course info
-		var courseInfo = $('.block_navigation .type_course li.type_unknown.depth_4');
-
-		//remove default navigation block
-		$('#region-pre .region-content').empty();
-		if($(tc).length || $(courseInfo).length) {
-			//add table of content
-			if($(tc).length && outline != null)
-				$('#region-pre .region-content').append('<div id="tableContents"><h4>' + outline + '</h4><ul>' + tc + '</ul></div>');	}
-
-		//go to link
-		$('#tableContents .tc').bind('click', function() {
-			var index = $(this).index();
-			var section = "#section-" + index;
-			$('body, html').animate({
-		  		scrollTop: $(section).offset().top
-			}, 500);
-			return false;
-		});
-	}
 }
 
 
@@ -431,7 +292,7 @@ function editMode(){
 	$('.block .commands').each(function () {
 		var p = $(this).parent();
 		if (p.find('h2').length == 0) {
-			p.prepend('<h2><img width="16" height="16" title="" alt="Edit block" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAKRJREFUeNpiZCAC+Pr6+wOpCUCsABV6AMQFmzdv3MhIhOZ4ILUAh3QAMwmaPwAxB5oSCyYiNV+AOh/dJQpMRGp2APr3IxAnohvCRKxmJCUFUO+AA5OJFM1AeX4gdQCIBWCGMZGh2QAqlACPRjI1LwQxGCnRDAtEsjWjx8ICJIVEaQYBFiT2BCjNSKxmXOnAnljN6C6AgQNIbLyasbqAFM0gABBgAFqOauNelbdAAAAAAElFTkSuQmCC" /></h2>');
+			p.prepend('<h2 class="empty-title"></h2>');
 		}
 	});
 }
